3ea2ccb20d9b72c5bbd352c8af8437c4aff49b19
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-07-02 12:17:06 +0200

    Send data to api

---
+++ index.js
@@ -0,0 +1,24 @@
+import colorSchemeMode from './colorSchemeMode.js'
+
+const header = document.querySelector('header')
+const hexEl = document.querySelector('input[type="color"]')
+const modeEl = document.createElement('select')
+const getColorBtn = document.querySelector('button')
+
+getColorBtn.addEventListener('click', () => {
+  let hexCode = hexEl.value.slice(1)
+  let mode = modeEl.value
+
+  fetch(`https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${mode}`)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+})
+
+colorSchemeMode.forEach((color) => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color.charAt(0).toUpperCase() + color.slice(1)
+  modeEl.appendChild(option)
+})
+
+header.insertBefore(modeEl, getColorBtn)
