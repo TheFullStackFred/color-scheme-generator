3fa4b243a1c776a9b4702a85d62e17c403aeaf00
TheFullStackFred <fredrikstromberg90@gmail.com>
2023-07-02 14:30:22 +0200

    Working application

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-import colorSchemeMode from './colorSchemeMode.js'
+import { colorSchemeModes, defaultHexAndMode } from './colorSchemeModes.js'
 
 const header = document.querySelector('header')
 const hexEl = document.querySelector('input[type="color"]')
 const modeEl = document.createElement('select')
 const getColorBtn = document.querySelector('button')
+const mainEl = document.querySelector('main')
+const footer = document.querySelector('footer')
 
 getColorBtn.addEventListener('click', () => {
   let hexCode = hexEl.value.slice(1)
   let mode = modeEl.value
+  console.log(hexCode, mode)
+  mainEl.innerHTML = ''
+  footer.textContent = ''
 
+  getColorScheme(hexCode, mode, renderColorScheme)
+})
+
+function createModeOptions() {
+  colorSchemeModes.forEach((color) => {
+    const option = document.createElement('option')
+    option.value = color
+    option.textContent = color.charAt(0).toUpperCase() + color.slice(1)
+    modeEl.appendChild(option)
+  })
+}
+
+function getColorScheme(hexCode, mode) {
   fetch(`https://www.thecolorapi.com/scheme?hex=${hexCode}&mode=${mode}`)
     .then((res) => res.json())
     .then((data) => {
-      data.colors.forEach((color) => {
-        console.log(color.hex.value)
-
-        const div = document.createElement('div')
-        div.style.background = `${color.hex.value}`
-        document.querySelector('main').appendChild(div)
-      })
+      renderColorScheme(data.colors)
     })
-})
+}
 
-colorSchemeMode.forEach((color) => {
-  const option = document.createElement('option')
-  option.value = color
-  option.textContent = color.charAt(0).toUpperCase() + color.slice(1)
-  modeEl.appendChild(option)
-})
+function renderColorScheme(colors) {
+  colors.forEach((color) => {
+    const colorEl = document.createElement('div')
+    colorEl.style.background = `${color.hex.value}`
+    const hexCodeEl = document.createElement('p')
+    hexCodeEl.textContent = `${color.hex.value}`
+    footer.appendChild(hexCodeEl)
+    mainEl.appendChild(colorEl)
+  })
+}
 
+function renderDefaultColorSchema() {
+  const hexCode = defaultHexAndMode.hex
+  const mode = defaultHexAndMode.mode
+  getColorScheme(hexCode, mode)
+}
+
+createModeOptions()
+renderDefaultColorSchema()
 header.insertBefore(modeEl, getColorBtn)
